e111feada162a6bd07683838c8b2c9fa2fc9d901
dhananjayaaps <dhananjayaaps@gmail.com>
2024-02-03 10:01:44 +0530

    added google authentication

--- Routes/AuthRoute.js
+++ Routes/AuthRoute.js
@@ -2,9 +2,17 @@ const { Signup, Login } = require('../Controllers/AuthController')
 const { userVerification} = require('../Middleware/AuthMiddleware')
 
 const router = require('express').Router()
+const passport = require('passport');
 
-router.post('/signup', Signup)
-router.post('/login', Login)
-router.post('/', userVerification)
+router.post('/signup', Signup);
+router.post('/login', Login);
+router.post('/', userVerification);
 
-module.exports = router
+// Google authentication routes
+router.get('/google', passport.authenticate('google', { scope: ['profile', 'email'] }));
+router.get('/google/callback', passport.authenticate('google', { failureRedirect: '/' }), (req, res) => {
+  // Successful authentication, redirect to the home page or handle it as needed
+  res.redirect('/');
+});
+
+module.exports = router;
